7b950d4a89bfc8b99424786f13f21f3ad3b7913c
Laura BL <laurabl_2@hotmail.com>
2021-05-19 11:08:45 +0200

    Añadido código para Reserva

--- FreeTourCuenca/src/app/booking/booking.component.ts
+++ FreeTourCuenca/src/app/booking/booking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
 
-  constructor() { }
+  mForm: FormGroup
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+    private router: Router) {
+
+      this.mForm = this.fb.group({
+        name: ["", Validators.required],
+        email: ["", Validators.required],
+        seats: ["", Validators.required],
+        date: ["", Validators.required],
+        time: ["", Validators.required],
+      })
+    }
+
+
+  ngOnInit() {
+    let today = new Date;
   }
 
+
+  get f(): any {
+    return this.mForm.controls
+  }
+
+ saveTickets() {
+      // aquí función para enviar mail a Jesús y confirmación a asistentes
+
+    }
 }
