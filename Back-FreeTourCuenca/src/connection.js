a3c3232717b62bdff1ea4f01dd278f8e883a5d70
JorgeVillaplana <jorge.villaplana.m@gmail.com>
2021-05-26 19:13:45 +0200

    Arreglado connection

--- Back-FreeTourCuenca/src/connection.js
+++ Back-FreeTourCuenca/src/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
-const password = "patatC0caC0la"
-const dbname = "jwt"
-const user = "admin"
-const host = "freetourcluster.mkygh.mongodb.net"
+const password = process.env.DBPASSWORD
+const dbname = process.env.DBNAME
+const user = process.env.DBUSER
+const host = process.env.DBHOST
 const uri = `mongodb+srv://${user}:${password}@${host}/${dbname}?retryWrites=true&w=majority`
 
 module.exports = mongoose.connect(uri, {
